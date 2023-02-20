ff57c30aed1e2d8e494aba7806ff83505826125c
Marcelo Gutierrez <m.gutierrezzamorano1@gmail.com>
2023-02-20 17:28:07 -0300

    HeroesApp

---
+++ src/app/auth/pages/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent  {
+
+  constructor(private router:Router,
+              private authService:AuthService) { }
+
+  login() {
+
+    // Ir al backend
+    // un usuario
+    this.authService.login()
+      .subscribe( (resp:any) => {
+        console.log(resp);
+
+        if ( resp.id ) {
+          this.router.navigate(['./heroes']);
+        }
+      })
+  }
+}
